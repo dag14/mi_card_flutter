57a0c9a0325915feabcae2bd50b700a63db2d8d4
dag14 <dagimasrat3@gmail.com>
2023-12-11 14:12:19 +0300

    added linkedin and github icons

---
+++ .dart_tool/flutter_build/d6c9da0e42438404cda25f79ab33f0f7/native_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\dagim\\AndroidStudioProjects\\mi_card_flutterrr\\.dart_tool\\flutter_build\\d6c9da0e42438404cda25f79ab33f0f7\\native_assets.yaml: 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,41 +10,118 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/gurd.jpg'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/gurd.jpg'),
+              ),
+              Text(
+                'Dagim Asrat',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  'Dagim Asrat',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSans3',
+                  fontSize: 20.0,
+                  color: Colors.yellow,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                color: Colors.yellow,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    '+251-913-101-699',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSans3',
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'SourceSans3',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade100,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                color: Colors.yellow,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'dagimasrat3@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSans3',
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-                Container(
-
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                color: Colors.yellow,
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.linkedin,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'dagimasrat',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSans3',
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
-              ],
-            ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                color: Colors.yellow,
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.github,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'dag14',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSans3',
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
